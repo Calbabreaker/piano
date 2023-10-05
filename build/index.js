1ce5bb74fe275b3f80f6c3d98ad4c4f545160e4b
Calbabreaker <calbabreaker@gmail.com>
2023-10-05 14:53:49 +0000

    Deploy at 2cc8cd0

--- build/index.js
+++ build/index.js
@@ -8,7 +8,7 @@ const dotenv_1 = require("dotenv");
 (0, dotenv_1.config)();
 const PORT = (_a = process.env.PORT) !== null && _a !== void 0 ? _a : 5000;
 const server = (0, http_1.createServer)().listen(PORT, "0.0.0.0", () => {
-    console.log(`Server running on ${PORT}.`);
+    console.log(`Server running on 0.0.0.0:${PORT}.`);
 });
 const io = new socket_io_1.Server(server, {
     cors: {
@@ -21,17 +21,22 @@ const instrumentNameMap = {};
 instrument_names_1.instrumentNames.forEach((name) => {
     instrumentNameMap[name] = true;
 });
+// A map of a room to client datas
 const roomClientMap = new Map();
 io.on("connection", (socket) => {
     var _a, _b;
     const roomName = (_a = socket.handshake.query) === null || _a === void 0 ? void 0 : _a.roomName;
     const instrumentName = (_b = socket.handshake.query) === null || _b === void 0 ? void 0 : _b.instrumentName;
-    // If the queries are not valid then immediatly disconnect the user since that should not be happening
+    function kick(message = "Something is wrong with the client") {
+        socket.emit("error_message", message);
+        socket.disconnect();
+    }
+    // If the queries are not valid then immediately disconnect the user since that should not be happening
     if (typeof roomName !== "string" ||
         roomName.length > 100 ||
         typeof instrumentName !== "string" ||
         !instrumentNameMap[instrumentName]) {
-        socket.disconnect();
+        kick();
         return;
     }
     if (!roomClientMap.has(roomName)) {
@@ -39,12 +44,11 @@ io.on("connection", (socket) => {
     }
     const connectedClients = roomClientMap.get(roomName);
     if (connectedClients.length > 50) {
-        socket.emit("error_message", "Room has over 50 people already!");
-        socket.disconnect();
+        kick("Room has over 50 people already!");
         return;
     }
     socket.join(roomName);
-    // Send the list of clients in this room when connected
+    // Send the list of clients in this room to only the connecting client so it knows the people here
     socket.emit("client_list_recieve", connectedClients);
     const clientData = {
         colorHue: Math.round(Math.random() * 360).toString(),
@@ -55,14 +59,16 @@ io.on("connection", (socket) => {
     io.to(roomName).emit("client_connect", clientData);
     // These listeners listen for a client to send that event and then broadcasts it to all other clients in the current room
     // If any of the sent data is invalid it immediately kicks the client
-    socket.on("instrument_change", (event) => {
+    socket.on("change_instrument", (event) => {
         const instrumentEvent = event;
-        if (!instrumentNameMap[instrumentEvent === null || instrumentEvent === void 0 ? void 0 : instrumentEvent.instrumentName]) {
-            socket.disconnect();
+        if (instrumentNameMap[instrumentEvent === null || instrumentEvent === void 0 ? void 0 : instrumentEvent.instrumentName]) {
+            instrumentEvent.socketID = socket.id;
+            clientData.instrumentName = instrumentEvent.instrumentName;
+            socket.to(roomName).emit("change_instrument", instrumentEvent);
         }
-        instrumentEvent.socketID = socket.id;
-        clientData.instrumentName = instrumentEvent.instrumentName;
-        socket.to(roomName).emit("instrument_change", instrumentEvent);
+        else {
+            kick();
+        }
     });
     socket.on("play_note", (event) => {
         const noteEvent = event;
@@ -71,7 +77,7 @@ io.on("connection", (socket) => {
             socket.to(roomName).emit("play_note", noteEvent);
         }
         else {
-            socket.disconnect();
+            kick();
         }
     });
     socket.on("stop_note", (event) => {
@@ -81,13 +87,15 @@ io.on("connection", (socket) => {
             socket.to(roomName).emit("stop_note", noteEvent);
         }
         else {
-            socket.disconnect();
+            kick();
         }
     });
     socket.on("disconnect", () => {
+        // Find the client and remove it
         const index = connectedClients.findIndex((client) => client.socketID == socket.id);
-        if (index !== -1)
+        if (index !== -1) {
             connectedClients.splice(index, 1);
+        }
         if (connectedClients.length === 0) {
             roomClientMap.delete(roomName);
         }
