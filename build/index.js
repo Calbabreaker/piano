e70b7fa8e66398c1cd0e37280bab9323ba30e258
Calbabreaker <calbabreaker@gmail.com>
2024-05-16 00:19:05 +0000

    Deploy at 0938b7a

--- build/index.js
+++ build/index.js
@@ -1,18 +1,17 @@
 "use strict";
-var _a, _b;
 Object.defineProperty(exports, "__esModule", { value: true });
 const socket_io_1 = require("socket.io");
 const http_1 = require("http");
 const instrument_names_1 = require("./instrument_names");
 const dotenv_1 = require("dotenv");
 (0, dotenv_1.config)();
-const PORT = (_a = process.env.PORT) !== null && _a !== void 0 ? _a : 5000;
+const PORT = process.env.PORT ?? 5000;
 const server = (0, http_1.createServer)().listen(PORT, "0.0.0.0", () => {
     console.log(`Server running on 0.0.0.0:${PORT}.`);
 });
 const io = new socket_io_1.Server(server, {
     cors: {
-        origin: (_b = process.env.CORS_ALLOW) !== null && _b !== void 0 ? _b : "*",
+        origin: process.env.CORS_ALLOW ?? "*",
         methods: ["GET", "POST"],
     },
 });
@@ -24,9 +23,8 @@ instrument_names_1.instrumentNames.forEach((name) => {
 // A map of a room to client datas
 const roomClientMap = new Map();
 io.on("connection", (socket) => {
-    var _a, _b;
-    const roomName = (_a = socket.handshake.query) === null || _a === void 0 ? void 0 : _a.roomName;
-    const instrumentName = (_b = socket.handshake.query) === null || _b === void 0 ? void 0 : _b.instrumentName;
+    const roomName = socket.handshake.query?.roomName;
+    const instrumentName = socket.handshake.query?.instrumentName;
     function kick(message = "Something is wrong with the client") {
         socket.emit("error_message", message);
         socket.disconnect();
@@ -61,7 +59,7 @@ io.on("connection", (socket) => {
     // If any of the sent data is invalid it immediately kicks the client
     socket.on("change_instrument", (event) => {
         const instrumentEvent = event;
-        if (instrumentNameMap[instrumentEvent === null || instrumentEvent === void 0 ? void 0 : instrumentEvent.instrumentName]) {
+        if (instrumentNameMap[instrumentEvent?.instrumentName]) {
             instrumentEvent.socketID = socket.id;
             clientData.instrumentName = instrumentEvent.instrumentName;
             socket.to(roomName).emit("change_instrument", instrumentEvent);
@@ -72,7 +70,7 @@ io.on("connection", (socket) => {
     });
     socket.on("play_note", (event) => {
         const noteEvent = event;
-        if (typeof (noteEvent === null || noteEvent === void 0 ? void 0 : noteEvent.note) === "string" && typeof (noteEvent === null || noteEvent === void 0 ? void 0 : noteEvent.volume) === "number") {
+        if (typeof noteEvent?.note === "string" && typeof noteEvent?.volume === "number") {
             noteEvent.socketID = socket.id;
             socket.to(roomName).emit("play_note", noteEvent);
         }
@@ -82,7 +80,7 @@ io.on("connection", (socket) => {
     });
     socket.on("stop_note", (event) => {
         const noteEvent = event;
-        if (typeof (noteEvent === null || noteEvent === void 0 ? void 0 : noteEvent.note) === "string" && typeof (noteEvent === null || noteEvent === void 0 ? void 0 : noteEvent.sustain) === "boolean") {
+        if (typeof noteEvent?.note === "string" && typeof noteEvent?.sustain === "boolean") {
             noteEvent.socketID = socket.id;
             socket.to(roomName).emit("stop_note", noteEvent);
         }
